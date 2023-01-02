7e4e62bd2c3c249f51a9d3258227e91b99a04eb4
Sanambir Singh <sanambir123@gmail.com>
2023-01-02 12:47:15 +0530

    set up api using custom fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
+import { useEffect } from 'react';
+import { getPosts } from '../api';
 
 function App() {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getPosts();
+      console.log('response', response);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        Hello world from React!
-      </header>
+      <header className="App-header">Hello world from React!</header>
     </div>
   );
 }
